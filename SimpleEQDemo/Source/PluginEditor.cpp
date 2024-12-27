89f014a13d69e63ca8d9352bd8fbafc40f12ef76
Bryan Santini <bryansantini@yahoo.com>
2024-12-26 20:39:12 -0500

    Added params to UI

--- SimpleEQDemo/Source/PluginEditor.cpp
+++ SimpleEQDemo/Source/PluginEditor.cpp
@@ -30,7 +30,7 @@ void SimpleEQDemoAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Hello World!!!!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void SimpleEQDemoAudioProcessorEditor::resized()
